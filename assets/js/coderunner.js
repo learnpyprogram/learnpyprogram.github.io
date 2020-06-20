0245fb04e94290e02829b8dcb30105d428549c86
jessechancy <jessec1@andrew.cmu.edu>
2020-06-20 08:34:47 +0800

    update box sizes

--- assets/js/coderunner.js
+++ assets/js/coderunner.js
@@ -29,3 +29,18 @@ function runboi(inpt, out) {
 function run1() {
     runboi("yourcode", "output");
 }
+const txHeight = 16;
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+  if (tx[i].value == '') {
+    tx[i].setAttribute("style", "height:" + txHeight + "px;overflow-y:hidden;");
+  } else {
+    tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  }
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput(e) {
+  this.style.height = "auto";
+  this.style.height = (this.scrollHeight) + "px";
+}
